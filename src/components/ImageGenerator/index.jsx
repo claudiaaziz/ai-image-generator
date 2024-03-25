5f54c2f8ab96e6f7e82f03283f760b935f99415c
claudiaaziz <claudiahannahh@gmail.com>
2024-03-24 20:40:58 -0400

    Add loading bar to improve UX

--- src/components/ImageGenerator/index.jsx
+++ src/components/ImageGenerator/index.jsx
@@ -3,12 +3,14 @@ import defaultImg from "../assets/default_image.svg"
 import { useRef, useState } from 'react';
 
 const ImageGenerator = () => {
-  const [imgUrl, setimgUrl] = useState("")
+  const [imgUrl, setImgUrl] = useState("")
+  const [loading, setLoading] = useState(false)
   const inputRef = useRef(null)
 
   const imgGenerator = async () => {
     if (inputRef.current.value === "") return
     
+    setLoading(true)
     const apiKey = process.env.REACT_APP_OPENAI_API_KEY
 
     const res = await fetch("https://api.openai.com/v1/images/generations", {
@@ -26,7 +28,8 @@ const ImageGenerator = () => {
     })
 
     const data = await res.json()
-    setimgUrl(data.data[0].url)
+    setImgUrl(data.data[0].url)
+    setLoading(false)
   }
 
   return (
@@ -34,6 +37,10 @@ const ImageGenerator = () => {
       <div className="header">AI image <span>generator</span></div>
       <div className="img-loading">
         <div className="image"><img src={imgUrl === "" ? defaultImg : imgUrl} alt="default" /></div>
+        <div className="loading">
+          <div className={loading ? "loading-bar-full loading-bar" : "loading-bar"}></div>
+          <div className={loading ? "loading-text" : "display-none"}>Loading...</div>
+        </div>
       </div>
       <div className="search-box">
         <input type="search" ref={inputRef} className='search-input' placeholder='Describe What You Want To See' />
